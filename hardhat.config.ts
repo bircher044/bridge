a159574476d69d113ba58ced4117e053474512b9
Oleksii Bereza <95891690+bircher044@users.noreply.github.com>
2022-10-09 01:51:00 +0300

    deploy scripts added

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,6 +7,16 @@ export default {
     hardhat: {
       chainId: 1337
     },
+    goerli: {
+      url: `https://eth-goerli.infura.io/v3/eaa3051a438c45149e629eb028072632`,
+      accounts: ["bfe15ac25c9a0686a721d1b2611649f27ae7aed8d7ff3409b7e7cd2ff473c33e"],
+      chainId: 5
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/eaa3051a438c45149e629eb028072632`,
+      accounts: ["bfe15ac25c9a0686a721d1b2611649f27ae7aed8d7ff3409b7e7cd2ff473c33e"],
+      chainId: 80001
+    },
     localhost: {
       chainId: 1337
     }
